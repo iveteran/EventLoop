7fc1956bf542870b0f872e6bccbd053faf51a4a5
iveteran <iveteran.yuu@gmail.com>
2015-07-14 09:20:03 +0800

    Inproved echoclient

--- echoclient.cc
+++ echoclient.cc
@@ -1,15 +1,41 @@
 #include <stdio.h>
+#include <tuple>
+
 #include "tcp_client.h"
+
+namespace richinfo {
+
+class BusinessTester {
+    public:
+    BusinessTester() : echoclient_("localhost", 22222)
+    {
+        echoclient_.SetOnMessageCb(OnMessageCallback(this, &BusinessTester::EchoClientMsgHandler2));
+
+        echoclient_.Send("hello");
+    }
+
+    protected:
+    void EchoClientMsgHandler(std::tuple<TcpConnection*, const string*> conn_msg_tuple)
+    {
+        TcpConnection* conn = std::get<0>(conn_msg_tuple);
+        const string* msg = std::get<1>(conn_msg_tuple);
+
+        printf("[echoclient] fd: %d, message: %s\n", conn->FD(), msg->c_str());
+    }
+    void EchoClientMsgHandler2(TcpConnection* conn, const string* msg)
+    {
+        printf("[echoclient] fd: %d, message: %s\n", conn->FD(), msg->c_str());
+    }
+
+    private:
+    TcpClient echoclient_;
+};
+}   // ns richinfo
 
 using namespace richinfo;
 
 int main(int argc, char **argv) {
-  TcpClient echoclient("localhost", 22222);
-  //echoclient.SetOnMessageCb(OnMessageCallback(this, &BusinessTester::EchoClientMsgHandler));
-  //echoclient.Connect();
-  echoclient.Send("hello");
-
-  //el.AddEvent(&echoclient);
+  BusinessTester biz_tester;
 
   EV_Singleton->StartLoop();
 
