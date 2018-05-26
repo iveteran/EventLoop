bcde78bb0bf1bb362d35d2f3ffaae34acdd92544
iveteran <iveteran.yuu@gmail.com>
2018-05-26 17:57:57 +0800

    [amqp_api] enabled SSL & refactor

--- plugin/amqp_api/amqp_client.cpp
+++ plugin/amqp_api/amqp_client.cpp
@@ -1,7 +1,23 @@
+#include <openssl/ssl.h>
 #include "amqp_client.h"
 
 namespace amqp_api
 {
+
+AMQPClient::AMQPClient(const char* address, bool auto_reconnect) :
+  amqp_address_(address), auto_reconnect_(auto_reconnect),
+  reconnect_timer_(std::bind(&AMQPClient::OnReconnectTimer, this, std::placeholders::_1))
+{
+  if (auto_reconnect_) {
+    reconnect_timer_.SetInterval(TimeVal(1, 0));
+  }
+
+  if (strncmp(address, "amqps://", 8) == 0) {
+    SSL_library_init();
+  }
+
+  BuildChannel();
+}
 
 void AMQPClient::DeclareExchange(const char* exchange, AMQP::ExchangeType exchangeType, int flag)
 {
@@ -35,6 +51,126 @@ void AMQPClient::Consume(const char* queue_name)
       std::bind(&AMQPClient::onConsumeSuccess, this, std::placeholders::_1)
       ).onMessage(
         std::bind(&AMQPClient::onConsumeMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+}
+
+void AMQPClient::BuildChannel()
+{
+  amqp_connection_ = std::make_shared<AMQP::TcpConnection>(this, amqp_address_);
+  channel_ = std::make_shared<AMQP::TcpChannel>(amqp_connection_.get());
+}
+
+void AMQPClient::DestoryChannel()
+{
+  channel_ = nullptr;
+  amqp_connection_ = nullptr;
+}
+
+void AMQPClient::Reconnect()
+{
+  DestoryChannel();
+  BuildChannel();
+}
+
+void AMQPClient::StartReconnectTimer()
+{
+  if (!reconnect_timer_.IsRunning())
+    reconnect_timer_.Start();
+}
+
+void AMQPClient::StopReconnectTimer()
+{
+  if (reconnect_timer_.IsRunning())
+    reconnect_timer_.Stop();
+}
+
+void AMQPClient::OnReconnectTimer(TimerEvent* timer)
+{
+  printf("[OnReconnectTimer begin] is ready: %d\n", IsReady());
+  if (!IsReady()) {  // if the connection is not created, then reconnect
+    Reconnect();
+  } else {
+    timer->Stop();
+  }
+}
+
+uint16_t AMQPClient::onNegotiate(AMQP::TcpConnection *connection, uint16_t interval)
+{
+  printf("[AMQPClient::onNegotiate]\n");
+  return 0;
+}
+
+void AMQPClient::onHeartbeat(AMQP::TcpConnection* connection)
+{
+  printf("[AMQPClient::onHeartbeat]\n");
+}
+
+void AMQPClient::onConnected(AMQP::TcpConnection *connection)
+{
+  printf("[AMQPClient::onConnected]\n");
+  if (auto_reconnect_) {
+    StopReconnectTimer();
+  }
+  amqp_callbacks_->on_connected_cb(this);
+}
+
+void AMQPClient::onClosed(AMQP::TcpConnection* connection)
+{
+  printf("[AMQPClient::onClosed]\n");
+  amqp_callbacks_->on_disconnected_cb(this);
+  if (auto_reconnect_) {
+    StartReconnectTimer();
+  }
+}
+
+void AMQPClient::onError(AMQP::TcpConnection* connection, const char* errmsg)
+{
+  printf("[AMQPClient::onError] %s\n", errmsg);
+  if (connection->fileno() < 0)
+  {
+    onClosed(connection);
+  }
+  else
+  {
+    amqp_callbacks_->on_error_cb(this, -1, errmsg);
+  }
+}
+
+void AMQPClient::onBindQueueSuccess()
+{
+  printf("[AMQPClient::onBindQueueSuccess]\n");
+}
+
+void AMQPClient::onBindQueueError(const char* errmsg)
+{
+  printf("[AMQPClient::onBindQueueError] errmsg: %s\n", errmsg);
+}
+
+void AMQPClient::onDeclareExchangeSuccess()
+{
+  printf("[AMQPClient::onDeclareExchangeSuccess]\n");
+}
+
+void AMQPClient::onDeclareExchangeError(const char* errmsg)
+{
+  printf("[AMQPClient::onDeclareExchangeError] errmsg: %s\n", errmsg);
+}
+
+void AMQPClient::onDeclareQueueSuccess(const std::string &queue_name, uint32_t msg_count, uint32_t consumer_count)
+{
+  printf("[AMQPClient::onDeclareQueueSuccess] queue_name: %s, msg_count: %d, consumer_count: %d\n", queue_name.c_str(), msg_count, consumer_count);
+}
+
+void AMQPClient::onConsumeSuccess(const std::string &tag)
+{
+  printf("[AMQPClient::onConsumeSuccess] tag: %s\n", tag.c_str());
+}
+
+void AMQPClient::onConsumeMessage(const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
+{
+  printf("[AMQPClient::onConsumeMessage] deliveryTag: %ld\n", deliveryTag);
+  AMQPMessage amqp_msg(&message);
+  amqp_callbacks_->on_message_cb(this, &amqp_msg);
+  channel_->ack(deliveryTag);
 }
 
 }  // namespace amqp_api
--- plugin/amqp_api/amqp_client.h
+++ plugin/amqp_api/amqp_client.h
@@ -28,16 +28,8 @@ class AMQPMessage
 class AMQPClient : public AMQPEventHandler
 {
   public:
-    AMQPClient(const char* address, bool auto_reconnect = true) :
-      amqp_address_(address), auto_reconnect_(auto_reconnect),
-      reconnect_timer_(std::bind(&AMQPClient::OnReconnectTimer, this, std::placeholders::_1))
-    {
-      if (auto_reconnect_) {
-        reconnect_timer_.SetInterval(TimeVal(1, 0));
-      }
+    AMQPClient(const char* address, bool auto_reconnect = true);
 
-      BuildChannel();
-    }
     bool IsReady() { return amqp_connection_->fileno() > 0; }
 
     void SetCallbacks(const AMQPCallbacksPtr& amqp_cbs) { amqp_callbacks_ = amqp_cbs; }
@@ -51,122 +43,27 @@ class AMQPClient : public AMQPEventHandler
     void Consume(const char* queue_name);
 
   private:
-    void BuildChannel()
-    {
-      amqp_connection_ = std::make_shared<AMQP::TcpConnection>(this, amqp_address_);
-      channel_ = std::make_shared<AMQP::TcpChannel>(amqp_connection_.get());
-    }
-    void DestoryChannel()
-    {
-      channel_ = nullptr;
-      amqp_connection_ = nullptr;
-    }
-    void Reconnect()
-    {
-      DestoryChannel();
-      BuildChannel();
-    }
-    void StartReconnectTimer()
-    {
-      if (!reconnect_timer_.IsRunning())
-        reconnect_timer_.Start();
-    }
-    void StopReconnectTimer()
-    {
-      if (reconnect_timer_.IsRunning())
-        reconnect_timer_.Stop();
-    }
-
-    void OnReconnectTimer(TimerEvent* timer)
-    {
-      printf("[OnReconnectTimer begin] is ready: %d\n", IsReady());
-      if (!IsReady()) {  // if the connection is not created, then reconnect
-        Reconnect();
-      } else {
-        timer->Stop();
-      }
-    }
+    void BuildChannel();
+    void DestoryChannel();
+    void Reconnect();
+    void StartReconnectTimer();
+    void StopReconnectTimer();
+    void OnReconnectTimer(TimerEvent* timer);
 
   protected:
-    virtual uint16_t onNegotiate(AMQP::TcpConnection *connection, uint16_t interval) override
-    {
-      printf("[AMQPClient::onNegotiate]\n");
-      return 0;
-    }
+    virtual uint16_t onNegotiate(AMQP::TcpConnection *connection, uint16_t interval) override;
+    virtual void onHeartbeat(AMQP::TcpConnection* connection) override;
+    virtual void onConnected(AMQP::TcpConnection *connection) override;
+    virtual void onClosed(AMQP::TcpConnection* connection) override;
+    virtual void onError(AMQP::TcpConnection* connection, const char* errmsg) override;
 
-    virtual void onHeartbeat(AMQP::TcpConnection* connection) override
-    {
-      printf("[AMQPClient::onHeartbeat]\n");
-    }
-
-    virtual void onConnected(AMQP::TcpConnection *connection) override
-    {
-      printf("[AMQPClient::onConnected]\n");
-      if (auto_reconnect_) {
-        StopReconnectTimer();
-      }
-      amqp_callbacks_->on_connected_cb(this);
-    }
-
-    virtual void onClosed(AMQP::TcpConnection* connection) override
-    {
-      printf("[AMQPClient::onClosed]\n");
-      amqp_callbacks_->on_disconnected_cb(this);
-      if (auto_reconnect_) {
-        StartReconnectTimer();
-      }
-    }
-
-    virtual void onError(AMQP::TcpConnection* connection, const char* errmsg) override
-    {
-      printf("[AMQPClient::onError] %s\n", errmsg);
-      if (connection->fileno() < 0)
-      {
-        onClosed(connection);
-      }
-      else
-      {
-        amqp_callbacks_->on_error_cb(this, -1, errmsg);
-      }
-    }
-
-    void onBindQueueSuccess()
-    {
-      printf("[AMQPClient::onBindQueueSuccess]\n");
-    }
-
-    void onBindQueueError(const char* errmsg)
-    {
-      printf("[AMQPClient::onBindQueueError] errmsg: %s\n", errmsg);
-    }
-
-    void onDeclareExchangeSuccess()
-    {
-      printf("[AMQPClient::onDeclareExchangeSuccess]\n");
-    }
-
-    void onDeclareExchangeError(const char* errmsg)
-    {
-      printf("[AMQPClient::onDeclareExchangeError] errmsg: %s\n", errmsg);
-    }
-
-    void onDeclareQueueSuccess(const std::string &queue_name, uint32_t msg_count, uint32_t consumer_count)
-    {
-      printf("[AMQPClient::onDeclareQueueSuccess] queue_name: %s, msg_count: %d, consumer_count: %d\n", queue_name.c_str(), msg_count, consumer_count);
-    }
-
-    void onConsumeSuccess(const std::string &tag)
-    {
-      printf("[AMQPClient::onConsumeSuccess] tag: %s\n", tag.c_str());
-    }
-
-    void onConsumeMessage(const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
-    {
-      printf("[AMQPClient::onConsumeMessage] deliveryTag: %ld\n", deliveryTag);
-      AMQPMessage amqp_msg(&message);
-      amqp_callbacks_->on_message_cb(this, &amqp_msg);
-      channel_->ack(deliveryTag);
-    }
+    void onBindQueueSuccess();
+    void onBindQueueError(const char* errmsg);
+    void onDeclareExchangeSuccess();
+    void onDeclareExchangeError(const char* errmsg);
+    void onDeclareQueueSuccess(const std::string &queue_name, uint32_t msg_count, uint32_t consumer_count);
+    void onConsumeSuccess(const std::string &tag);
+    void onConsumeMessage(const AMQP::Message &message, uint64_t deliveryTag, bool redelivered);
 
   private:
     AMQP::Address         amqp_address_;
